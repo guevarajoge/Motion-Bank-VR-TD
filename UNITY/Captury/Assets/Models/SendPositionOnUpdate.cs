803c0a3df2f4808e2b0d4fe3126f65dd95dcb113
jorgeGuevara <jorge.guevara@student.ehb.be>
2022-03-31 19:38:56 +0200

    feature/unity

---
+++ UNITY/Captury/Assets/Models/SendPositionOnUpdate.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SendPositionOnUpdate : MonoBehaviour {
+
+	public OSC osc;
+    public string address;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	  OscMessage message = new OscMessage();
+
+        message.address = name != null ? "/" + name : address;
+        message.values.Add(transform.position.x);
+        message.values.Add(transform.position.y);
+        message.values.Add(transform.position.z);
+        osc.Send(message);
+
+    }
+
+
+}
